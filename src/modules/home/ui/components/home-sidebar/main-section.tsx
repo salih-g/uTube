ae99068c3ffdf9974d000571a9239f6a714187ad
Muhammed Salih Gümüş <gumusmuhammedsalih@gmail.com>
2025-03-05 23:25:44 +0300

    add basic auth pages

--- src/modules/home/ui/components/home-sidebar/main-section.tsx
+++ src/modules/home/ui/components/home-sidebar/main-section.tsx
@@ -1,6 +1,7 @@
 'use client';
 import Link from 'next/link';
 import { FlameIcon, HomeIcon, PlaySquareIcon } from 'lucide-react';
+import { useAuth, useClerk } from '@clerk/nextjs';
 
 import {
   SidebarGroup,
@@ -30,6 +31,9 @@ const items = [
 ];
 
 export const MainSection = () => {
+  const clerk = useClerk();
+  const { isSignedIn } = useAuth();
+
   return (
     <SidebarGroup>
       <SidebarGroupContent>
@@ -40,7 +44,12 @@ export const MainSection = () => {
                 tooltip={item.title}
                 asChild
                 isActive={false}
-                onClick={() => {}}
+                onClick={(e) => {
+                  if (!isSignedIn && item.auth) {
+                    e.preventDefault();
+                    return clerk.openSignIn();
+                  }
+                }}
               >
                 <Link href={item.url} className='flex items-center gap-4 '>
                   <item.icon />
